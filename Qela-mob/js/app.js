29e96e4163995e54bb00ebb5eaafdb022125fd29
“Andrey <foresteraws@gmail.com>
2025-03-09 12:34:28 +0200

    Added chat page + fix icons font

--- Qela-mob/js/app.js
+++ Qela-mob/js/app.js
@@ -47,4 +47,16 @@ window.onload = function () {
 // 	observer.observe(navbarTop);
 // }
 // console.log(navbarTop);
-3
+3;
+
+const btnChatBottom = document.querySelector('.qela-chat-btn');
+const navBottom = document.querySelector('.qela-bottom')
+const observer = new ResizeObserver(entrirs => {
+	const elem = entrirs[0];
+	console.log(elem);
+	let heightAside = elem.borderBoxSize[0].blockSize;
+	btnChatBottom.style.bottom = heightAside + 'px';
+	console.log(btnChatBottom.style.bottom = heightAside + 'px');
+
+})
+observer.observe(navBottom);
